082b852ccfcf10998806409208dc3a116dd7463c
GeetanshGandhi <geetansh.gandhi2504@gmail.com>
2025-09-04 23:38:47 +0530

    commit #16 : updating persisting job

--- src/main/java/com/project/veriphi/utils/PersistentJob.java
+++ src/main/java/com/project/veriphi/utils/PersistentJob.java
@@ -1,15 +1,18 @@
 package com.project.veriphi.utils;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@Component
+@RestController
+@RequestMapping("/health")
 public class PersistentJob {
 
     private static long no = 0;
-    @Scheduled(fixedRate = 5 * 60 * 1000)
+
+    @GetMapping("/check")
     public void powerUp(){
         log.info("persisting health check {}...", no);
         no++;
